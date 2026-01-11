e3483b930f736e1113fc86724364a6ff8aaa65e8
yuubann <yban1385@gmail.com>
2026-01-11 23:33:41 +0900

    Update Task.java

--- JavaTodoApp/Task.java
+++ JavaTodoApp/Task.java
@@ -1,28 +1,28 @@
-public class Task{
+public class Task {
 
- private String title;
- private boolean done;
+    private String title;
+    private boolean done;
 
- public Task(String title){
-  this.title = title;
-  this.done = false;
- }
+    public Task(String title) {
+        this.title = title;
+        this.done = false;
+    }
 
- public void markDone(){
-  this.done = true;
- }
+    public void markDone() {
+        this.done = true;
+    }
 
- public boolean isDone(){
-  return done;
- }
+    public boolean isDone() {
+        return done;
+    }
 
- public String getTitle(){
-  return title;
- }
+    public String getTitle() {
+        return title;
+    }
 
- @Override
- public String toString(){
-  return(done ? "[×]" : "[ ]") + title;
+    @Override
+    public String toString() {
+        return (done ? "[x]" : "[ ]") + title;
 
-}
+    }
 }
